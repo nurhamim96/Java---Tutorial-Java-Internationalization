05cefd72b5803e5a4be43d23d42a6139b8bbb4aa
ibad nurhamim <nurhamimibad96@gmail.com>
2021-03-08 13:52:19 +0700

    Add Message Format Type and Choice Format

--- src/test/java/com/tutorial/i18n/ChoiceFormatTest.java
+++ src/test/java/com/tutorial/i18n/ChoiceFormatTest.java
@@ -1,2 +1,62 @@
-package com.tutorial.i18n;public class ChoiceFormatTest {
+package com.tutorial.i18n;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.ChoiceFormat;
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class ChoiceFormatTest {
+
+    @Test
+    void testChoiceFormat() {
+        String pattern = "-1#Negative | 0#Kosong | 1#Satu | 1<Banyak";
+        ChoiceFormat choiceFormat = new ChoiceFormat(pattern);
+
+        System.out.println(choiceFormat.format(-2));
+        System.out.println(choiceFormat.format(-1));
+        System.out.println(choiceFormat.format(0));
+        System.out.println(choiceFormat.format(1));
+        System.out.println(choiceFormat.format(2));
+        System.out.println(choiceFormat.format(3));
+    }
+
+    @Test
+    void testChoiceFormatInMessageFormat() {
+        Locale locale = new Locale("in", "ID");
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("message", locale);
+
+        String pattern = resourceBundle.getString("balance");
+
+        MessageFormat messageFormat = new MessageFormat(pattern, locale);
+        System.out.println(messageFormat.format(new Object[]{-1000000}));
+        System.out.println(messageFormat.format(new Object[]{0}));
+        System.out.println(messageFormat.format(new Object[]{1000000}));
+    }
+
+    @Test
+    void testChoiceFormatInMessageFormatUSA() {
+        Locale locale = new Locale("en", "US");
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("message", locale);
+
+        String pattern = resourceBundle.getString("balance");
+
+        MessageFormat messageFormat = new MessageFormat(pattern, locale);
+        System.out.println(messageFormat.format(new Object[]{-1000000}));
+        System.out.println(messageFormat.format(new Object[]{0}));
+        System.out.println(messageFormat.format(new Object[]{1000000}));
+    }
 }
+
+
+/**
+ * NOTE :
+ *
+ * ChoiceFormat class adalah class yang digunakan untuk menyematkan data dengan range number, biasanya digunakan untuk kata plurals, misal jika kita namanya file, jika banyak namanya files.
+ * Pembuatan pattern untuk ChoiceFormat sangat sederhana, cukup gunakan number#value atau number<value, dan urutannya harus dari kecil ke besar, dan dipisahkan menggunakan tanda |(pagar),
+ * misal: -1#negatif | 0#kosong | 1#satu | 1<banyak
+ * Link list country codes : https://www.iban.com/country-codes.
+ * Link list language codes ISO 639.2 : https://loc.gov/standards/iso639-2/php/code_list.php.
+ *
+ */
